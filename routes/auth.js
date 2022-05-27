0fd8e04a389463314a4468d660f68bc77cddf734
Jared Hanson <jaredhanson@gmail.com>
2022-05-27 16:02:30 -0700

    Update app and routes.

--- routes/auth.js
+++ routes/auth.js
@@ -1,87 +1,83 @@
 var express = require('express');
 var passport = require('passport');
-var idp = require('../idp');
+var GoogleStrategy = require('passport-google-oidc');
+var FacebookStrategy = require('passport-facebook');
+var TwitterStrategy = require('passport-twitter');
 var db = require('../db');
 
 
-function singleSignOn(req, res, next) {
-  // TODO: need to set at req.federatedAuthInfo;
-  var state = req.authInfo.state || {};
-  var action = state.action || 'login';
-  
-  if (action !== 'login') { return next(); }
-  
+function jitProvision(provider, profile, cb) {
   db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
-    'https://' + req.params.provider,
-    req.federatedUser.id
+    provider,
+    profile.id
   ], function(err, row) {
-    if (err) { return next(err); }
+    if (err) { return cb(err); }
     if (!row) {
       db.run('INSERT INTO users (name) VALUES (?)', [
-        req.federatedUser.displayName
+        profile.displayName
       ], function(err) {
-        if (err) { return next(err); }
-        
+        if (err) { return cb(err); }
         var id = this.lastID;
-        db.run('INSERT INTO federated_credentials (provider, subject, user_id) VALUES (?, ?, ?)', [
-          'https://' + req.params.provider,
-          req.federatedUser.id,
-          id
+        db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
+          id,
+          provider,
+          profile.id
         ], function(err) {
-          if (err) { return next(err); }
+          if (err) { return cb(err); }
           var user = {
-            id: id.toString(),
-            displayName: req.federatedUser.displayName
+            id: id,
+            name: profile.displayName
           };
-          req.login(user, function(err) {
-            if (err) { return next(err); }
-            res.redirect('/');
-          });
+          return cb(null, user);
         });
       });
     } else {
-      db.get('SELECT rowid AS id, username, name FROM users WHERE rowid = ?', [ row.user_id ], function(err, row) {
-        if (err) { return next(err); }
-  
-        // TODO: Handle undefined row.
-        var user = {
-          id: row.id.toString(),
-          username: row.username,
-          displayName: row.name
-        };
-        req.login(user, function(err) {
-          if (err) { return next(err); }
-          res.redirect('/');
-        });
+      db.get('SELECT * FROM users WHERE id = ?', [ row.user_id ], function(err, row) {
+        if (err) { return cb(err); }
+        if (!row) { return cb(null, false); }
+        return cb(null, row);
       });
     }
   });
 }
 
-function accountLink(req, res, next) {
-  console.log('ACCOUNT LINK!');
-  console.log(req.user);
-  console.log(req.federatedUser);
-  console.log(req.authInfo);
-  
-  
-  // TODO: need to set at req.federatedAuthInfo;
-  var state = req.authInfo.state || {};
-  var action = state.action;
-  
-  if (action !== 'link') { return next(); }
-  
-  // TODO: Check that state user is req user.
-  
-  db.run('INSERT INTO federated_credentials (provider, subject, user_id) VALUES (?, ?, ?)', [
-    'https://' + req.params.provider,
-    req.federatedUser.id,
-    req.user.id
-  ], function(err) {
-    if (err) { return next(err); }
-    res.redirect(state.returnTo || '/');
+passport.use(new GoogleStrategy({
+  clientID: process.env['GOOGLE_CLIENT_ID'],
+  clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
+  callbackURL: '/oauth2/redirect/google',
+  scope: [ 'profile' ]
+}, function verify(issuer, profile, cb) {
+  return jitProvision(issuer, profile, cb);
+}));
+
+passport.use(new FacebookStrategy({
+  clientID: process.env['FACEBOOK_CLIENT_ID'],
+  clientSecret: process.env['FACEBOOK_CLIENT_SECRET'],
+  callbackURL: '/oauth2/redirect/facebook',
+  state: true
+}, function verify(accessToken, refreshToken, profile, cb) {
+  return jitProvision('https://www.facebook.com', profile, cb);
+}));
+
+passport.use(new TwitterStrategy({
+  consumerKey: process.env['TWITTER_CONSUMER_KEY'],
+  consumerSecret: process.env['TWITTER_CONSUMER_SECRET'],
+  callbackURL: '/oauth/callback/twitter'
+}, function verify(token, tokenSecret, profile, cb) {
+  return jitProvision('https://twitter.com', profile, cb);
+}));
+
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    cb(null, { id: user.id, username: user.username, name: user.name });
   });
-}
+});
+
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
 
 
 var router = express.Router();
@@ -90,31 +86,32 @@ router.get('/login', function(req, res, next) {
   res.render('login');
 });
 
-router.get('/login/federated/:provider',
-  function(req, res, next) {
-    var strategy = idp.create(req.params.provider);
-    passport.authenticate(strategy)(req, res, next);
+router.get('/login/federated/google', passport.authenticate('google'));
+
+router.get('/oauth2/redirect/google', passport.authenticate('google', {
+  successReturnToOrRedirect: '/',
+  failureRedirect: '/login'
+}));
+
+router.get('/login/federated/facebook', passport.authenticate('facebook'));
+
+router.get('/oauth2/redirect/facebook', passport.authenticate('facebook', {
+  successReturnToOrRedirect: '/',
+  failureRedirect: '/login'
+}));
+
+router.get('/login/federated/twitter', passport.authenticate('twitter'));
+
+router.get('/oauth/callback/twitter', passport.authenticate('twitter', {
+  successReturnToOrRedirect: '/',
+  failureRedirect: '/login'
+}));
+
+router.post('/logout', function(req, res, next) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
   });
-
-router.get('/oauth2/redirect/:provider',
-  function(req, res, next) {
-    var strategy = idp.create(req.params.provider);
-    passport.authenticate(strategy, { assignProperty: 'federatedUser', failureRedirect: '/login' })(req, res, next);
-  },
-  singleSignOn,
-  accountLink);
-  
-router.get('/oauth/callback/:provider',
-  function(req, res, next) {
-    var strategy = idp.create(req.params.provider);
-    passport.authenticate(strategy, { assignProperty: 'federatedUser', failureRedirect: '/login' })(req, res, next);
-  },
-  singleSignOn,
-  accountLink);
-
-router.get('/logout', function(req, res, next) {
-  req.logout();
-  res.redirect('/');
 });
 
 module.exports = router;
