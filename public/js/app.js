d57e00ac1850a420f3310647bb6f16c9fb8424da
Jared Hanson <jaredhanson@gmail.com>
2022-05-31 12:12:49 -0700

    Clean up.

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,30 @@
+// https://www.quirksmode.org/js/cookies.html
+function createCookie(name,value,days) {
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime()+(days*24*60*60*1000));
+    var expires = "; expires="+date.toGMTString();
+  }
+  else var expires = "";
+  document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+function eraseCookie(name) {
+  createCookie(name,"",-1);
+}
+
 window.addEventListener('load', function() {
-  
-  // https://www.quirksmode.org/js/cookies.html
-  function createCookie(name,value,days) {
-    if (days) {
-      var date = new Date();
-      date.setTime(date.getTime()+(days*24*60*60*1000));
-      var expires = "; expires="+date.toGMTString();
-    }
-    else var expires = "";
-    document.cookie = name+"="+value+expires+"; path=/";
-  }
-  
-  function readCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-      var c = ca[i];
-      while (c.charAt(0)==' ') c = c.substring(1,c.length);
-      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
-  }
-  
-  function eraseCookie(name) {
-    createCookie(name,"",-1);
-  }
-  
   
   var fc = readCookie('fc');
   eraseCookie('fc');
@@ -42,9 +41,9 @@ window.addEventListener('load', function() {
     
     var credential = new FederatedCredential(data);
     navigator.credentials.store(credential)
-      .catch(function(error) {
-        console.log(error)
-      });
+    .catch(function(error) {
+      console.log(error)
+    });
   }
   
 });
